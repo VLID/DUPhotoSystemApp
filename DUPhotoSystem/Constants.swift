debd58e887347581c4f3c5390faba8094675d3db
VLID <vinceluoindenver@gmail.com>
2016-10-31 01:00:11 -0600

    Sign in ui custom classes complete

---
+++ DUPhotoSystem/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DUPhotoSystem
+//
+//  Created by lcocox on 2016/10/31.
+//  Copyright © 2016年 lcocox. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
